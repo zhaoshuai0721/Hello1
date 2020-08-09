f46862ac3b375a7e353560776158ca70bc633f58
赵帅 <244532049@qq.com>
2020-08-09 11:41:31 +0800

    hello第一次

---
+++ src/main/java/hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("不好好听课揍你");
+    }
+    public void hello(){
+        System.out.println("不好好听课揍你");
+    }
+}
